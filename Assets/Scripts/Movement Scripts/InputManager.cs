841ecb39550e38ff817d4e81551e8258e24d5c66
FabianDim <63860191+FabianDim@users.noreply.github.com>
2023-10-30 16:47:47 +1100

    Make pacstudent rotate

--- Assets/Scripts/Movement Scripts/InputManager.cs
+++ Assets/Scripts/Movement Scripts/InputManager.cs
@@ -9,8 +9,8 @@ public class InputManager : MonoBehaviour
 
     private Tweener tweener;
     private Vector3 first = new Vector3(1152f, 541f, 0.0f);
-    private Vector3 second = new Vector3(1152f, 786f, 0.0f);
-    private Vector3 third = new Vector3(141.6f, 786f, 0.0f);
+    private Vector3 second = new Vector3(1152f, 795f, 0.0f);
+    private Vector3 third = new Vector3(141.6f, 795f, 0.0f);
     
     void Start()
     {
@@ -32,16 +32,20 @@ public class InputManager : MonoBehaviour
             tweener.AddTween(borderItems.transform, borderItems.transform.position, new Vector3(1152f, 541f, 0.0f), 10f);
             if(borderItems.transform.position.Equals(first))
             {
-                tweener.AddTween(borderItems.transform, borderItems.transform.position, new Vector3(1152f, 786f, 0.0f), 3f);
+                borderItems.transform.rotation = Quaternion.Euler(0, 0, 180);
+                tweener.AddTween(borderItems.transform, borderItems.transform.position, new Vector3(1152f, 795f, 0.0f), 3f);
             }
             if(borderItems.transform.position.Equals(second))
             {
-                tweener.AddTween(borderItems.transform, borderItems.transform.position, new Vector3(141.6f, 786f, 0.0f), 10f);
+                
+                tweener.AddTween(borderItems.transform, borderItems.transform.position, new Vector3(141.6f, 795f, 0.0f), 10f);
             }
             if(borderItems.transform.position.Equals(third))
             {
+                
                 tweener.AddTween(borderItems.transform, borderItems.transform.position, new Vector3(141.6f, 541f, 0.0f), 3f);
             }
+            
 
         }
         
