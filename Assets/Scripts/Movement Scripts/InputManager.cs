25f11d3efa0b10c82e1d2f9d8d3c564af66819f2
FabianDim <63860191+FabianDim@users.noreply.github.com>
2023-10-31 22:44:19 +1100

    Animated border as good as it gets

--- Assets/Scripts/Movement Scripts/InputManager.cs
+++ Assets/Scripts/Movement Scripts/InputManager.cs
@@ -3,25 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private GameObject borderItems;
+    
 
     private Tweener tweener;
-    private Vector3 first = new Vector3(1152f, 541f, 0.0f);
-    private Vector3 second = new Vector3(1152f, 795f, 0.0f);
-    private Vector3 third = new Vector3(141.6f, 795f, 0.0f);
+    private Vector3 first = new Vector3(504.8f, 116, 0.0f);
+    private Vector3 second = new Vector3(504.8f, 368.7f, 0.0f);
+    private Vector3 third = new Vector3(-502.5f, 368.7f, 0.0f);
     
-    private Quaternion rightRotation = Quaternion.Euler(0, 0, 0);
-    private Quaternion upRotation = Quaternion.Euler(0, 0, 90);
-    private Quaternion leftRotation = Quaternion.Euler(0, 0, 180);
-    private Quaternion downRotation = Quaternion.Euler(0, 0, 270);
-
     void Start()
     {
         tweener = GetComponent<Tweener>();
-        borderItems.transform.rotation = rightRotation;
+
+        
     }
 
     private void Update()
@@ -29,30 +25,29 @@ public class InputManager : MonoBehaviour
         Move();
     }
 
+
     void Move()
     {
         if (tweener.activeTween == null)
         {
-            if (borderItems.transform.position == first)
+            tweener.AddTween(borderItems.transform, borderItems.transform.position, new Vector3(1073f, 480.4593f, 0.0f), 10f);
+            if(borderItems.transform.position.Equals(first))
             {
-                tweener.AddTween(borderItems.transform, borderItems.transform.position, second, 3f);
-                borderItems.transform.rotation = upRotation;
+                tweener.AddTween(borderItems.transform, borderItems.transform.position, new Vector3(504.8f, 368.7f, 0.0f), 3f);
             }
-            else if (borderItems.transform.position == second)
+            if(borderItems.transform.position.Equals(second))
             {
-                tweener.AddTween(borderItems.transform, borderItems.transform.position, third, 10f);
-                borderItems.transform.rotation = leftRotation;
+                
+                tweener.AddTween(borderItems.transform, borderItems.transform.position, new Vector3(-502.5f, 795f, 0.0f), 10f);
             }
-            else if (borderItems.transform.position == third)
+            if(borderItems.transform.position.Equals(third))
             {
-                tweener.AddTween(borderItems.transform, borderItems.transform.position, first, 3f);
-                borderItems.transform.rotation = downRotation;
+                
+                tweener.AddTween(borderItems.transform, borderItems.transform.position, new Vector3(141.6f, 368.7f, 0.0f), 3f);
             }
-            else
-            {
-                tweener.AddTween(borderItems.transform, borderItems.transform.position, first, 10f);
-                borderItems.transform.rotation = rightRotation;
-            }
+            
+
         }
+        
     }
 }
--- Assets/Scripts/UI Scripts/UI Manager.cs
+++ Assets/Scripts/UI Scripts/UI Manager.cs
@@ -11,11 +11,19 @@ public class UIManager : MonoBehaviour
         SceneManager.sceneLoaded += OnSceneLoaded;
         SceneManager.LoadScene(0);
     }
+    public void QuitGame()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.LoadScene(1);
+        // UnityEditor.EditorApplication.isPlaying = false;
+    }
     public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         if (scene.buildIndex == 1)
         {
             Button Level1 = GameObject.FindWithTag("Level1").GetComponent<Button>();
+            Button exit = GameObject.FindWithTag("QuitButton").GetComponent<Button>();
+            exit.onClick.AddListener(QuitGame);
         }
     }
     void Start()
